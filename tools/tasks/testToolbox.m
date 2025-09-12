6dd2ea189a6e576ed5e0ab8a56802b47ec2fc058
ehennestad <ehennestad@gmail.com>
2025-09-12 21:33:56 +0200

    Update testToolbox.m

--- tools/tasks/testToolbox.m
+++ tools/tasks/testToolbox.m
@@ -1,5 +1,7 @@
 function testToolbox(varargin)
     projectRootDir = omkgsynctools.projectdir();
     matbox.installRequirements(fullfile(projectRootDir))
+    addpath(genpath('MATLAB-AddOnds'))
+    ls
     matbox.tasks.testToolbox(projectRootDir, varargin{:})
 end
