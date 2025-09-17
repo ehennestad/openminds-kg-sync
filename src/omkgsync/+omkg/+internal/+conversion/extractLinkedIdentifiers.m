cee9c8c35c2cebb270e4ac43daa3278fe26f62d6
ehennestad <ehennestad@gmail.com>
2025-09-17 14:11:58 +0200

    Update extractLinkedIdentifiers.m

--- src/omkgsync/+omkg/+internal/+conversion/extractLinkedIdentifiers.m
+++ src/omkgsync/+omkg/+internal/+conversion/extractLinkedIdentifiers.m
@@ -1,4 +1,9 @@
 function linkedIdentifiers = extractLinkedIdentifiers(metadataNode, linkedIdentifiers)
+% extractLinkedIdentifiers - Extract identifiers for forward linked nodes
+%
+%   Given a metadata node, will extract all forward links and append to a
+%   list of linked identifiers. 
+
     arguments
         metadataNode
         linkedIdentifiers (1,:) string = string.empty
@@ -10,25 +15,23 @@ function linkedIdentifiers = extractLinkedIdentifiers(metadataNode, linkedIdenti
         currentField = fields{i};
         currentValue = metadataNode.(currentField);
         if ~isempty(currentValue)
+
             if openminds.utility.isInstance(currentValue)
-                if currentValue.State == "reference"
-                    linkedIdentifiers(end+1) = {currentValue.id}; %#ok<AGROW>
+                for j = 1:numel(currentValue)
+                    if currentValue(j).State == "reference"
+                        linkedIdentifiers(end+1) = {currentValue(j).id}; %#ok<AGROW>
+                    end
                 end
             elseif openminds.utility.isMixedInstance(currentValue)
                 % TODO: Handle mixed instances
-            elseif isstruct(currentValue) && isfield(currentValue, 'id') % TODO: This should not be a struct!
-                linkedIdentifiers = [linkedIdentifiers, string({currentValue.id})]; %#ok<AGROW>
+                error('OMKG:ExtractLinkedIds:NotImplemented', ...
+                    ['Internal error - Extracting linked ids from mixed ', ...
+                    'type instances is not supported yet, please report if ', ...
+                    'you see this error.'])
+            elseif isstruct(currentValue) && isfield(currentValue, 'x_id') % TODO: This should not be a struct!
+                linkedIdentifiers = [linkedIdentifiers, string({currentValue.x_id})]; %#ok<AGROW>
             end
         end
-
-        % if strcmp(currentField, 'at_id')
-        %     linkedIdentifiers(end+1) = metadataNode.("at_id"); %#ok<AGROW>
-        % else
-        %     if isstruct(metadataNode.(currentField))
-        %         currentNode = metadataNode.(currentField);
-        %         linkedIdentifiers = omkg.internal.conversion.extractLinkedIdentifiers(currentNode, linkedIdentifiers);
-        %     end
-        % end
     end
     linkedIdentifiers = unique(linkedIdentifiers);
 end
