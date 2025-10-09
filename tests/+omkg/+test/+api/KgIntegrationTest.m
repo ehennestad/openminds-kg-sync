e44acbb8afcf159465975b5da98171b59e87ce8e
ehennestad <ehennestad@gmail.com>
2025-10-09 17:00:16 +0200

    Create KgIntegrationTest.m

---
+++ tests/+omkg/+test/+api/KgIntegrationTest.m
@@ -0,0 +1,19 @@
+classdef KgIntegrationTest < matlab.unittest.TestCase
+    
+    methods (TestClassSetup)
+        function setTestPreferences(testCase)
+            currentServerPref = omkg.getpref("DefaultServer");
+            omkg.setpref("DefaultServer", "PROD");
+            testCase.addTeardown(@() omkg.setpref("DefaultServer", currentServerPref))
+        end
+    end
+
+    methods (Test, TestTags={'LiveIntegration'})
+        function testList(testCase)
+            p = kglist("Person", "from", 100, "size", 20, "space", "common");
+
+            testCase.verifyClass(p, 'openminds.core.Person')
+            testCase.verifyLength(p, 20)
+        end
+    end
+end
