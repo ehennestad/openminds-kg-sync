474e0a119f9f8c9409e23f61d91f6c9b90be18bc
ehennestad <ehennestad@gmail.com>
2026-06-17 11:15:11 +0200

    Update removeNamespaceIRIFromPropertyNames.m
    Update openMINDS property IRI to support removing namespace IRI from  instances of openMINDS v4 and above

--- src/omkg/+omkg/+internal/+conversion/removeNamespaceIRIFromPropertyNames.m
+++ src/omkg/+omkg/+internal/+conversion/removeNamespaceIRIFromPropertyNames.m
@@ -3,8 +3,15 @@ function metadataNode = removeNamespaceIRIFromPropertyNames(metadataNode)
     arguments
         metadataNode (1,1) struct
     end
+    
+    OPENMINDS_IRI = [...
+        "https___openminds_ebrains_eu_vocab_", ... % openMINDS Version 3 and below
+        "https___openminds_om_i_org_props_" ];    % openMINDS Version 4 and above
 
-    OPENMINDS_IRI = "https___openminds_ebrains_eu_vocab_";
+    currentIRI = matlab.lang.makeValidName(openminds.constant.PropertyIRIPrefix);
+    assert(ismember(currentIRI, OPENMINDS_IRI), ...
+        'OMKG:RemoveNameSpaceIRI:CurrentVersionNotSupported', ...
+        'Internal Error: Current openMINDS version is not supported, please raise an issue.')
 
     propertyNames = fieldnames(metadataNode);
     for i = 1:numel(OPENMINDS_IRI)
